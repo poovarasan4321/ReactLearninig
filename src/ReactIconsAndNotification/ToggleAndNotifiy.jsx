fbed77b9f82c1354d06f9fb9eb8a624163cff53a
poovarasan4321 <ppoovarasan2927@gmail.com>
2026-05-18 11:49:36 +0530

    "REact icons and React-hot-toaster how install how use "

--- src/ReactIconsAndNotification/ToggleAndNotifiy.jsx
+++ src/ReactIconsAndNotification/ToggleAndNotifiy.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
 import { FaUserPlus } from "react-icons/fa";
 import { CiLock } from "react-icons/ci";
+import toast from 'react-hot-toast';
+import { FaEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa6";
+import { useState } from 'react';
+
 
 const ToggleAndNotifiy = () => {
 
 
+    const [showPassword, setShowPassword] = useState(false);
+
+    // let eye = () => {
+    // setShowPassword(!showPassword)
+    let eye = () => {
+
+        let input = document.getElementById("password");
+        let icon = document.getElementById("icon")
+
+        if (input.type === "password") {
+            input.type = "text";
+            // span.value =  <FaRegEyeSlash />
+             icon.innerHTML = "🙈";
+
+        }
+        else {
+            input.type = "password";
+        }
+    }
+
+    // }
+
     let handleSubmit = (e) => {
         e.preventDefault();
         console.log("sucess");
+        toast.success("Form Submitted Successfully")
+        // toast.error("SomeThing went wrong")
 
     }
 
@@ -17,7 +46,7 @@ const ToggleAndNotifiy = () => {
             <h1 className='text-3xl font-semibold font-mono'>Toggle and Notification</h1>
 
             <form action="" onSubmit={handleSubmit}
-                className='  border-3 border-amber-500 rounder-lg bg-blue-500 w-1/4   px-8 py-10 shadow-2xl flex flex-col gap-5'>
+                className='  border-1 rounded-xl border-amber-500 rounder-lg bg-blue-500 w-1/4   px-8 py-10 shadow-black flex flex-col gap-5'>
 
                 <div
                     className='w-full flex flex-col gap-2 relative'>
@@ -28,7 +57,7 @@ const ToggleAndNotifiy = () => {
 
                     <input
                         type="text"
-                        className='p-2 block outline-none pl-8 py-1 px-2 border border-white text-red-500 caret-red-500' />
+                        className='p-2 block outline-none rounded pl-8 py-1 px-2 border border-white text-red-950 caret-red-500' />
                     <span
                         className=' text-white text-2xl absolute top-10 left-1 '>
                         <FaUserPlus />
@@ -41,14 +70,22 @@ const ToggleAndNotifiy = () => {
                         htmlFor=""
                         className='text-xl p-2 text-white text-bolder '>Password</label>
                     <input
-                        type="password" className='block outline-none py-1 pl-8 px-2 border border-white text-red-500 caret-red-500 w-full' />
+                        // type={showPassword ? "text" : "password"}
+                        type="text"
+                        id="password"
+                        className='block outline-none py-1 pl-8 px-2 border border-white rounded text-red-950 caret-red-500 w-full' />
                     <span className='text-white text-2xl absolute top-14 left-1'><CiLock /></span>
+                    <span onClick={eye} 
+                    id="icon"
+                    className='absolute text-white right-2 top-15 cursor-pointer '>
+                        <FaEye />
+                    </span>
 
                 </div>
                 <div
                     className='w-full justify-center items-center'>
                     <button
-                        className='block outline-none py-1 px-2 border border-violet-600 text-white caret-red-500 bg-violet-600 hover:bg-violet-900 hover:cursor-pointer w-full'>
+                        className='block rounded outline-none py-1 px-2 border border-violet-600 text-white caret-red-500 bg-violet-600 hover:bg-violet-900 hover:cursor-pointer w-full'>
                         Login User
                     </button>
                 </div>
